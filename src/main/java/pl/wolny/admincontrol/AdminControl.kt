7f79477c5c1a3086362f31e26ceb7dbf268a6ce3
wolny <isupermichael007@gmail.com>
2022-06-07 17:44:37 +0200

    Setup kotlin

---
+++ src/main/java/pl/wolny/admincontrol/AdminControl.kt
@@ -0,0 +1,13 @@
+package pl.wolny.admincontrol
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AdminControl : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
